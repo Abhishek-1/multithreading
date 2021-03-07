69bc1c0075dccec69a4388ca463d46a6264db5a7
Abhishek Ranjan <abhi@Mohitas-MacBook-Pro.local>
2021-03-06 21:24:33 -0500

    Code change for implementing Thread Pool

---
+++ src/threadPoolWithThreadPoolExecutor/MonitorThread.java
@@ -0,0 +1,40 @@
+package threadPoolWithThreadPoolExecutor;
+
+import java.util.concurrent.ThreadPoolExecutor;
+
+public class MonitorThread extends Thread{
+
+  private ThreadPoolExecutor executor;
+  private int seconds;
+  private Boolean run=true;
+
+  public MonitorThread(ThreadPoolExecutor executor, int delay){
+    this.executor = executor;
+    this.seconds = delay;
+  }
+
+  @Override
+  public void run() {
+    while(run){
+      System.out.println(
+          String.format("[monitor] [%d/%d] Active: %d, Completed %d, Task %d, isShutdown: %s, isTerminated: %s",
+              executor.getPoolSize(),
+              executor.getCorePoolSize(),
+              executor.getActiveCount(),
+              executor.getCompletedTaskCount(),
+              executor.getTaskCount(),
+              executor.isShutdown(),
+              executor.isTerminated())
+      );
+      try {
+        Thread.sleep(5000);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    }
+  }
+
+  public void shutdown(){
+    this.run=false;
+  }
+}
---
+++ src/threadPoolWithThreadPoolExecutor/ThreadPoolExecutorFramework.java
@@ -0,0 +1,39 @@
+package threadPoolWithThreadPoolExecutor;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+public class ThreadPoolExecutorFramework {
+
+  public static void main(String[] args) throws InterruptedException {
+    RejectedExecutionHandlerImpl rejectionHandler = new RejectedExecutionHandlerImpl();
+    ThreadFactory threadFactory = Executors.defaultThreadFactory();
+    ThreadPoolExecutor executorPool = new ThreadPoolExecutor(
+        2,
+        4,
+        10,
+        TimeUnit.SECONDS,
+        new ArrayBlockingQueue<Runnable>(2),
+        threadFactory,
+        rejectionHandler);
+
+    /****** Monitoring ThreadPoolExecutor *****/
+    MonitorThread monitor = new MonitorThread(executorPool, 3);
+    monitor.start();
+
+    for(int i=0; i< 10; i++){
+      executorPool.execute(new WorkerThread("cmd"+i));
+    }
+
+    Thread.sleep(30000);
+    executorPool.shutdown();
+    Thread.sleep(5000);
+    monitor.shutdown();
+
+
+  }
+
+}
